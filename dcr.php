e5f7cbaf55a8e04e18b8d8ab3d2284e1778f89ee
AlkibiadesX <1362296+hrst2000@users.noreply.github.com>
2019-12-06 21:17:47 +0000

    remade dcr.php

--- dcr.php
+++ dcr.php
@@ -1,37 +1,34 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <script type="text/javascript" src="/js/dcr.js"></script>
+	<head>
+		<script type="text/javascript" src="js/dcr.js"></script>
+		<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+		<script type="text/javascript" src="js/protocolcheck.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/launch.css" />
+	</head>
+	<body>
+		<!-- Information -->
+		<div id="box">
+			<p>Make sure you have downloaded the Migoland Client from <a href='http://migo.spineworld.nl/Migoland-Setup.exe'>here!</a></p>
+			<p>After downloading and installing press 'play' below!</p>
+		</div>
+
+		<!-- The play button -->
+		<div id="protocol" href="mllauncher:hashhere"><img src="images/buttons/btn_play.png"></img></div>
 		
-		<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-		<script type="text/javascript" src="/js/protocolcheck.js"></script>
-		 <style>
-body {
-  background-image: url('images/background2.png');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  //background-size: cover;
-}
-</style> 
-    </head>
-    <body style="background-color:#94d13e; margin:0px; padding: 0px; overflow: hidden; display:block;">
-			<center><br><br><br><div id="no-launcher" style="display: block;">
-			Make sure you have downloaded the Migoland Launcher from<br>
-			<a href='http://migo.spineworld.nl/Migoland-Setup.exe'>Download</a><br>
-			After downloading and installing press Launch Migoland below!<br><div id="protocol" href="mllauncher:hashhere">Launch Migoland</div></div></center>
-            
 
-<script>
-var launcherid = document.getElementById('no-launcher');
-        $("#protocol").click(function (event) {
-            protocolCheck($(this).attr("href"), function () {
+		<!-- Migoland launch script -->
+		<script>
+		var launcherid = document.getElementById('no-launcher');
+		        $("#protocol").click(function (event) {
+		            protocolCheck($(this).attr("href"), function () {
 					launcherid.style.display = 'block';
-                });
-            event.preventDefault ? event.preventDefault() : event.returnValue = false;
+		});
+		event.preventDefault ? event.preventDefault() : event.returnValue = false;
 
-        });
+		});
 		$( "#protocol" ).trigger( "click" );
-</script>
-    </body>
-</html>
+		</script>
 
+	</body>
 </html>
